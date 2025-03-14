c2aa7c0ca78584f8557c11dab99e596cca4565b4
Abhishek K M <abhishekmallikarjunayya2k@gmail.com>
2025-03-14 22:13:49 +0530

    modified and added test file

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req,res)=>{res.send("hello world")});
-app.listen(PORT, () => {
+
+if(require.main === module){app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
-  
+  }
+
+module.exports = app;
