935c0ffa3b5c8eac5dec7c04988ed0884f0c31e3
Abhishek K M <abhishekmallikarjunayya2k@gmail.com>
2025-03-15 00:13:40 +0530

    commited changes

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../index");
+const app = require("./index");
 describe("GET /", () => {
   it("should return 'Hello, World!'", async () => {
     const res = await request(app).get("/");
