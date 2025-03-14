9a14fafa135b256c920dc2da6d19795d9c60e1ba
Abhishek K M <abhishekmallikarjunayya2k@gmail.com>
2025-03-15 00:22:42 +0530

    commited changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req,res)=>{res.send("hello world")});
+app.get("/", (req,res)=>{res.send("Hello, World!")});
 
 if(require.main === module){app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
